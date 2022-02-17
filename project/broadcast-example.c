52dedcbdade272ed521bdf5806e23f185aa9db74
Tim Grutzeck <tim.grutzeck@campus.tu-berlin.de>
2022-02-17 14:17:09 +0100

    better output and fixed bug that ack is send more than one time

--- project/broadcast-example.c
+++ project/broadcast-example.c
@@ -1,40 +1,3 @@
-/*
- * Copyright (c) 2007, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-/**
- * \file
- *         Testing the broadcast layer in Rime
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
 #include "contiki.h"
 #include "net/rime/rime.h"
 #include "random.h"
@@ -45,7 +8,6 @@
 #include "net/rime/timesynch.h"
 #include "sys/energest.h"
 
-/*---------------------------------------------------------------------------*/
 PROCESS(example_broadcast_process, "Broadcast example");
 PROCESS(energest_thread, "Energest Thread");
 AUTOSTART_PROCESSES(&example_broadcast_process, &energest_thread);
@@ -64,8 +26,7 @@ static int isPrintMode(int mode){
 }
 
 // address of the reporting point
-#define REPORTING_POINT_ADDR0 1
-#define REPORTING_POINT_ADDR1 0
+static const linkaddr_t REPORTING_POINT_ADDR = { {1,0,0,0, 0,0,0,0} };
 
 // packet types
 #define ALERT 0
@@ -118,9 +79,7 @@ static void sendAlert(struct broadcast_conn* broadcast){
         packet.packetID = ++lastPacketIdSend;
         packet.hops = 0;
 
-        linkaddr_copy(&(packet.receiver), &linkaddr_null);
-        packet.receiver.u8[0] = REPORTING_POINT_ADDR0;
-        packet.receiver.u8[1] = REPORTING_POINT_ADDR1;
+        linkaddr_copy(&(packet.receiver), &REPORTING_POINT_ADDR);
 
         packetbuf_copyfrom((void*)&packet, sizeof(Packet));
         
@@ -129,8 +88,8 @@ static void sendAlert(struct broadcast_conn* broadcast){
         /* Send broadcast packet */
         broadcast_send(broadcast);
         
-        if(isPrintMode(DEBUG))
-            printf("%d, %d sent broadcast packet\n", linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1]);
+        if(isPrintMode(LATENCY | DEBUG))
+            printf("%d sent alert packet [%d, %d]\n", linkaddr_node_addr.u8[0], packet.sender.u8[0], packet.packetID);
 }
 
 static void sendAck(struct broadcast_conn* broadcast, linkaddr_t* sender){
@@ -138,9 +97,7 @@ static void sendAck(struct broadcast_conn* broadcast, linkaddr_t* sender){
         Packet packet;
         packet.type = ACK;
         
-        linkaddr_copy(&(packet.sender), &linkaddr_null);
-        packet.sender.u8[0] = REPORTING_POINT_ADDR0;
-        packet.sender.u8[1] = REPORTING_POINT_ADDR1;
+        linkaddr_copy(&(packet.sender), &REPORTING_POINT_ADDR);
         packet.packetID = ++lastPacketIdSend;
         packet.hops = 0;
 
@@ -153,8 +110,8 @@ static void sendAck(struct broadcast_conn* broadcast, linkaddr_t* sender){
         /* Send broadcast packet */
         broadcast_send(broadcast);
         
-        if(isPrintMode(DEBUG))
-            printf("%d, %d sent ack broadcast packet\n", linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1]);
+        if(isPrintMode(LATENCY | DEBUG))
+            printf("%d sent ack packet [%d, %d]\n", linkaddr_node_addr.u8[0], packet.sender.u8[0], packet.packetID);
 }
 
 
@@ -164,21 +121,30 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from) {
     Packet* packet = (Packet*) packetbuf_dataptr();
     packet->hops++;
 
+    if(linkaddr_cmp(&linkaddr_node_addr, &packet->receiver)){
+        if(isPrintMode(LATENCY | DEBUG))
+            printf("%s received %s packet [%d, %d] from %d\n", packet->type == ACK ? "source" : "sink", packet->type == ACK ? "ack" : "alert", packet->sender.u8[0], packet->packetID, packet->sender.u8[0]);
+
+        if(isPrintMode(HOPS))
+            printf("number of hops: %d\n", packet->hops);
+    }
+
+    /* transmit packet only if it has not already been sent */
+    if(isPacketInList(&packetList, packet))
+        return;
+    packetbuf_copyfrom((void*)packet, sizeof(Packet));
+    addToPacketList(&packetList, packet);
+
+
     if(isPrintMode(DEBUG))
-            printf("broadcast message received from %d.%d: packed type %d, hops %d\n", from->u8[0], from->u8[1], packet->type, packet->hops);
+            printf("broadcast message received from %d: packed type %d, hops %d\n", from->u8[0], packet->type, packet->hops);
 
     // check if packet reached destination
     if(linkaddr_cmp(&linkaddr_node_addr, &packet->receiver)){
 
         if(packet->type == ALERT){
 
-            if(linkaddr_node_addr.u8[0] == REPORTING_POINT_ADDR0 && linkaddr_node_addr.u8[1] == REPORTING_POINT_ADDR1){
-                
-                if(isPrintMode(LATENCY))
-                    printf("sink received packet from %d, %d\n", packet->sender.u8[0], packet->sender.u8[1]);
-
-                if(isPrintMode(HOPS))
-                    printf("number of hops: %d\n", packet->hops);
+            if(linkaddr_cmp(&linkaddr_node_addr, &REPORTING_POINT_ADDR)){
 
                 // reporting point received packet: send acknowledgement back to sender
                 sendAck(c, &packet->sender);
@@ -188,42 +154,22 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from) {
 
         } else if (packet->type == ACK) {
 
-            if(isPrintMode(LATENCY))
-                printf("source received ack packet from %d, %d\n", packet->sender.u8[0], packet->sender.u8[1]);
-
             // source of alert received ackonwledgement: stop retransmission of packet
             lastPacketIdAck = lastPacketIdSend;
             
             return;
         }
     }
-
-    /* transmit packet only if it has not already been sent */
-    if(isPacketInList(&packetList, packet))
-        return;
-
-
-    // wait random time to avoid collision
-    /*static struct etimer et;
-    unsigned long ticks = CLOCK_SECOND / 1000 + random_rand() % (CLOCK_SECOND / 500);
-    etimer_set(&et, ticks);
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));*/
-
-    packetbuf_copyfrom((void*)packet, sizeof(Packet));
-        
-    addToPacketList(&packetList, packet);
     
     /* Send broadcast packet */
     broadcast_send(c);
     
     if(isPrintMode(DEBUG))
-        printf("%d, %d sent broadcast packet\n", linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1]);
+        printf("%d sent packet\n", linkaddr_node_addr.u8[0]);
 }
 
 static const struct broadcast_callbacks broadcast_cb = {broadcast_recv};
 
-/*---------------------------------------------------------------------------*/
-/* Broadcast connection */
 static struct broadcast_conn broadcast;
 
 PROCESS_THREAD(example_broadcast_process, ev, data) {
